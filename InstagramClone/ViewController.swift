0a154f3d402e3820e4e8af9bf03738fdd1a4759d
Devesh <tyagi77@outlook.com>
2019-09-10 11:25:50 +0530

    Initial Commit

---
+++ InstagramClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by Devesh Tyagi on 10/09/19.
+//  Copyright © 2019 Devesh Tyagi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
