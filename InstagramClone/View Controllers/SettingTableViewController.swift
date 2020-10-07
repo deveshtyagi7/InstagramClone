1f064bad5ff3065529c988cda03c238adf1982c0
Devesh Tyagi <tyagi77@outlook.com>
2020-10-07 22:44:58 +0530

    Fetch user data for setting

--- InstagramClone/View Controllers/SettingTableViewController.swift
+++ InstagramClone/View Controllers/SettingTableViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class SettingTableViewController: UITableViewController {
-
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var usernameTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         navigationItem.title = "Edit Profile"
+        fetchCurrentUser()
     }
-
-   
+    
+    func fetchCurrentUser(){
+        Api.User.observeCurrentUser { (user) in
+            self.usernameTextField.text = user.userName
+            self.emailTextField.text = user.email
+            if let profileURL = URL(string: user.profileImageUrl!){
+                self.profileImageView.sd_setImage(with: profileURL)
+            }
+        }
+    }
+    @IBOutlet weak var logoutBtnPressed: UIView!
+    
+    @IBOutlet weak var saveButtonPressed: UIView!
 }
