c2df2323f85dd16ea25cb8426c098c8f6caad5f7
Devesh Tyagi <tyagi77@outlook.com>
2020-10-11 00:22:11 +0530

    Video Picker And Thumbnail Generator

--- InstagramClone/View Controllers/CameraViewController.swift
+++ InstagramClone/View Controllers/CameraViewController.swift
@@ -8,24 +8,22 @@
 
 import UIKit
 import ProgressHUD
+import AVFoundation
 
-class CameraViewController: UIViewController ,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
-    
-    
-    
+class CameraViewController: UIViewController{
 
-    @IBOutlet weak var RemoveButton: UIBarButtonItem!
-    @IBOutlet weak var UploadPicture: UIImageView!
-    @IBOutlet weak var CaptionTextField: UITextView!
-    @IBOutlet weak var ShareButton: UIButton!
-    var SelectedImage : UIImage?
-    
+    @IBOutlet weak var removeButton: UIBarButtonItem!
+    @IBOutlet weak var uploadPicture: UIImageView!
+    @IBOutlet weak var captionTextField: UITextView!
+    @IBOutlet weak var shareButton: UIButton!
+    var selectedImage : UIImage?
+    var videoUrl : URL?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(SelectPicture))
-        UploadPicture.addGestureRecognizer(tapGesture)
-        UploadPicture.isUserInteractionEnabled =  true
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(selectPicture))
+        uploadPicture.addGestureRecognizer(tapGesture)
+        uploadPicture.isUserInteractionEnabled =  true
         
         
         
@@ -38,16 +36,16 @@ class CameraViewController: UIViewController ,UIImagePickerControllerDelegate,UI
     }
     
     func disableButton(){
-        if SelectedImage != nil{
-            self.ShareButton.isEnabled = true
-            self.RemoveButton.isEnabled = true
-            self.ShareButton.backgroundColor = .black
+        if selectedImage != nil{
+            self.shareButton.isEnabled = true
+            self.removeButton.isEnabled = true
+            self.shareButton.backgroundColor = .black
             //   self.CaptionTextField.text = "Write caption here"
         }
         else{
-            self.ShareButton.isEnabled = false
-            self.RemoveButton.isEnabled = false
-            self.ShareButton.backgroundColor = .darkGray
+            self.shareButton.isEnabled = false
+            self.removeButton.isEnabled = false
+            self.shareButton.backgroundColor = .darkGray
         }
     }
     
@@ -61,9 +59,9 @@ class CameraViewController: UIViewController ,UIImagePickerControllerDelegate,UI
     @IBAction func ShareButtonPressed(_ sender: Any) {
         view.endEditing(true)
         ProgressHUD.show("Posting")
-        if let profileImage = self.SelectedImage ,
+        if let profileImage = self.selectedImage ,
             let imageData = profileImage.jpegData(compressionQuality: 0.1){
-            HelperServices.uploadDataToServer(data: imageData, caption: CaptionTextField.text!, onSucess: {
+            HelperServices.uploadDataToServer(data: imageData, caption: captionTextField.text!, onSucess: {
                 self.clean()
                 self.tabBarController?.selectedIndex = 0
             })
@@ -78,25 +76,54 @@ class CameraViewController: UIViewController ,UIImagePickerControllerDelegate,UI
     
     
     
-    @objc func SelectPicture(){
+    @objc func selectPicture(){
         let pickerController = UIImagePickerController()
         pickerController.delegate =  self
+        pickerController.mediaTypes = ["public.image", "public.movie"]
         present(pickerController, animated: true, completion: nil )
     }
+    
+    func clean(){
+        self.captionTextField.text = ""
+        self.uploadPicture.image = UIImage(named: "placeholder-photo")
+        self.selectedImage = nil
+    }
+    
+}
+extension CameraViewController : UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let videoUrl = info[UIImagePickerController.InfoKey.mediaURL] as? URL{
+            if let thumbnail = self.thumbnailImageForFileUrl(videoUrl){
+                selectedImage = thumbnail
+                uploadPicture.image = thumbnail
+                self.videoUrl = videoUrl
+            }
+          
+        }
+        
         if let Image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
-            SelectedImage = Image
-            UploadPicture.image = Image
+            selectedImage = Image
+            uploadPicture.image = Image
             
         }
         
         dismiss(animated: true, completion: nil)
         disableButton()
     }
-    func clean(){
-        self.CaptionTextField.text = ""
-        self.UploadPicture.image = UIImage(named: "placeholder-photo")
-        self.SelectedImage = nil
-    }
     
+    func thumbnailImageForFileUrl(_ fileUrl : URL) -> UIImage? {
+        let asset = AVAsset(url: fileUrl)
+        let imageGenerator = AVAssetImageGenerator(asset: asset)
+        do {
+            let thumbnailCGImage = try imageGenerator.copyCGImage(at: CMTimeMake( value: 6 , timescale: 3), actualTime: nil)
+            return UIImage(cgImage: thumbnailCGImage)
+        } catch let err {
+            print(err)
+        }
+       
+        
+        return nil
+    }
 }
